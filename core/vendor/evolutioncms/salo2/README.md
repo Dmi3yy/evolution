cd045852ded55fb4b4396b31b8756b73bc0a0967
Dmytro Lukianenko <dmytro.lukianenko@sogema.com>
2024-12-25 19:34:01 +0100

    Up composer for fix salo

---
+++ core/vendor/evolutioncms/salo2/README.md
@@ -0,0 +1,13 @@
+## Evolution CMS - Salo package
+
+#use
+
+`php core/artisan salo:install` - for create docker-compose.yml file
+
+`./core/vendor/bin/salo up` - run docker demon mode
+
+`./core/vendor/bin/salo up -d` - run docker in demon mode
+
+`./core/vendor/bin/salo down` - stop docker
+
+`./core/vendor/bin/salo down -v` - stop docker and delete the existing Docker volume data
---
+++ core/vendor/evolutioncms/salo2/src/Console/PublishCommand.php
@@ -0,0 +1,43 @@
+<?php namespace EvolutionCMS\Salo\Console;
+
+use Illuminate\Console\Command;
+
+class PublishCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'salo:publish';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Publish the Evo Salo Docker files';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->call('vendor:publish', ['--tag' => 'salo']);
+
+        file_put_contents(
+            $this->laravel->basePath('docker-compose.yml'),
+            str_replace(
+                [
+                    './vendor/laravel/salo/runtimes/8.2',
+                ],
+                [
+                    './docker/8.2',
+                ],
+                file_get_contents($this->laravel->basePath('docker-compose.yml'))
+            )
+        );
+    }
+}
